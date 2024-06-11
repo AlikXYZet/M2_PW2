1096925f3f7a4c18bbdda81671bd8157a3129625
Александр Осипов <alikxyzet@gmail.com>
2024-06-11 20:10:27 +0300

    2.2 Adding a "Hash function" based on multiplication

--- PW2.cpp
+++ PW2.cpp
@@ -1,27 +1,49 @@
 ﻿#include <iostream>
 #include <iomanip>
 
+
+
+/* ---   Предварительное   --- */
 /* Количество элементов массива:
 * диапазон значений от 0 до 99 включительно
 */
 #define SIZE 100
 
-// «Хеш-функция», основанная на делении
-int hf_modulo(std::string inString)
+// Суммирование номеров элементов строки
+int sum(std::string inString)
+{
+	int sum = 0;
+	for (size_t i = 0; i < inString.length(); ++i)
+		sum += inString[i];
+	return sum;
+}
+// ---------------------------------------------------------------
+
+
+
+/* ---   Хеш-функции   --- */
+// «Хеш-функция», основанная на делении (модуле)
+unsigned int hf_mod(std::string inString)
 {
 	/* unsigned -- для исключения отрицательного значения,
 	*  например: ввод кирилицы
 	*/
-	unsigned int mod = 0;
-
-	for (size_t i = 0; i < inString.length(); ++i)
-	{
-		mod += inString[i];
-	}
-
-	mod %= SIZE;
+	unsigned int mod = sum(inString) % SIZE;
 	return mod;
 }
+
+// «Хеш-функция», основанная на умножении
+unsigned int hf_mul(std::string inString)
+{
+	/* unsigned -- для исключения отрицательного значения,
+	*  например: ввод кирилицы
+	*/
+	unsigned int mul = fmod(sum(inString) * 0.618033, 1) * SIZE;
+	return mul;
+}
+// ---------------------------------------------------------------
+
+
 
 int main()
 {
@@ -31,14 +53,14 @@ int main()
 	// Переменная вводимой информации
 	std::string cInput;
 
-	for (int8_t i = 0; i < 5; i++)
+	for (int8_t i = 0; i < 10; i++)
 	{
 		// Ввод
 		std::cin >> cInput;
 
-		// Вывод с шириной поля
-		std::cout << std::setw(4) << hf_modulo(cInput);
-		std::cout << "-- hf_modulo()";
+		// Вывод с фиксированной шириной поля
+		std::cout << std::setw(3) << hf_mod(cInput) << "-- hf_mod();    ";
+		std::cout << std::setw(3) << hf_mul(cInput) << "-- hf_mul()";
 		std::cout << std::endl;
 	}
 
